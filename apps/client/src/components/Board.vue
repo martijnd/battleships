8fb445a2f1e57106c6646859e1d02cc6453811f8
Martijn Dorsman <martijn.dorsman@gmail.com>
2024-09-27 19:20:36 +0200

    Add axis

--- apps/client/src/components/Board.vue
+++ apps/client/src/components/Board.vue
@@ -18,11 +18,20 @@ function coordIsHit(x: number, y: number) {
 
 <template>
   <h2>Board</h2>
-  <div class="grid grid-cols-10">
-    <div v-for="(_, row) in rows" :key="row">
-      <div v-for="(_, col) in cols" :key="col" :class="`border border-gray-500 h-8 w-8 flex items-center justify-center text-white font-bold ${coordIsHit(col, row) ? 'bg-red-500' : 'bg-blue-500'
-        }`" @click="$emit('fire', { x: col, y: row })">
-        {{ String.fromCharCode(65 + col) }}{{ row + 1 }}
+  <div class="flex">
+    <div class="flex flex-col">
+      <div class="h-8 w-8 flex items-center justify-center" v-for="number in 11">{{ number > 1 ? String.fromCharCode(64
+        +
+        number) : '' }}
+      </div>
+    </div>
+    <div class="grid grid-cols-10">
+      <div class="h-8 w-8 flex items-center justify-center" v-for="index in 10">{{ index }}</div>
+      <div v-for="(_, row) in rows" :key="row">
+        <div v-for="(_, col) in cols" :key="col" :class="`border border-gray-500 h-8 w-8 flex items-center justify-center text-white font-bold ${coordIsHit(col, row) ? 'bg-red-500' : 'bg-blue-500'
+          }`" @click="$emit('fire', { x: col, y: row })">
+          {{ String.fromCharCode(65 + col) }}{{ row + 1 }}
+        </div>
       </div>
     </div>
   </div>
